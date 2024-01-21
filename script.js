0adb549391bed8dbd3a970233f644212a3f238ab
engincanokyay <engincanokyay3535@gmail.com>
2024-01-21 23:54:52 +0300

    next js typing

--- script.js
+++ script.js
@@ -1,57 +1,81 @@
 const questions = [
     {
         question: "Which is the largest animal in the world?",
-        answer:[
-            { test:"Shark", correct: false },
-            { test:"Blue whale", correct: true },
-            { test:"Elephant", correct: false },
-            { test:"Giraffe", correct: false },
+        answers:[
+            { text:"Shark", correct: false },
+            { text:"Blue whale", correct: true },
+            { text:"Elephant", correct: false },
+            { text:"Giraffe", correct: false },
         ]
     },
     {
         question: "Which is the smallest country in the world?",
-        answer:[
-            { test:"Vatican City", correct: true },
-            { test:"Bhutan", correct: false },
-            { test:"Nepal", correct: false },
-            { test:"Shri Lanka", correct: false },
+        answers:[
+            { text:"Vatican City", correct: true },
+            { text:"Bhutan", correct: false },
+            { text:"Nepal", correct: false },
+            { text:"Shri Lanka", correct: false },
         ]
     },
     {
         question: "Which is the largest desert in the world?",
-        answer:[
-            { test:"Kalhari", correct: false },
-            { test:"Gobi", correct: false },
-            { test:"Sahara", correct: true },
-            { test:"Antarctica", correct: false },
+        answers:[
+            { text:"Kalhari", correct: false },
+            { text:"Gobi", correct: false },
+            { text:"Sahara", correct: true },
+            { text:"Antarctica", correct: false },
         ]
     },
     {
         question: "Which is the smallest continent in the world?",
-        answer:[
-            { test:"Asia", correct: false },
-            { test:"Australia", correct: true },
-            { test:"Arctic", correct: false },
-            { test:"Africa", correct: false },
+        answers:[
+            { text:"Asia", correct: false },
+            { text:"Australia", correct: true },
+            { text:"Arctic", correct: false },
+            { text:"Africa", correct: false },
         ]
     }
     
 ];
 
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
-const next = document.getElementById("next-btn");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
 
 function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
 
 }
 
-function showQuesstion(){
+function showQuestion(){
+    reserState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex +1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.
     question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
 }
+
+function reserState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild)
+    answerButtons.removeChild(answerButtons.firstChild);
+
+}
+startQuiz();
